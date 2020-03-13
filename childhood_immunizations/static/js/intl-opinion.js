954773ce24e0a82b20e590e55f106cd67cd61973
Fangyi Xu <fxu@caltech.edu>
2020-03-13 17:55:58 -0400

    mapsize

--- childhood_immunizations/static/js/intl-opinion.js
+++ childhood_immunizations/static/js/intl-opinion.js
@@ -1,6 +1,6 @@
 var divElement = document.getElementById('viz1583953293342');
 var vizElement = divElement.getElementsByTagName('object')[0];
-vizElement.style.width='1366px';vizElement.style.height='795px';   
+vizElement.style.width='1280px';vizElement.style.height='795px';   
 var scriptElement = document.createElement('script');
 scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
 vizElement.parentNode.insertBefore(scriptElement, vizElement);              
