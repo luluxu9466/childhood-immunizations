f9507dcda9debba4025485829dfd48c2ea9a08ed
Fangyi Xu <fxu@caltech.edu>
2020-03-13 22:55:39 -0400

    new maps

--- childhood_immunizations/static/js/us_map.js
+++ childhood_immunizations/static/js/us_map.js
@@ -3,4 +3,4 @@ var vizElement = divElement.getElementsByTagName('object')[0];
 vizElement.style.width='1250px';vizElement.style.height='795px';                    
 var scriptElement = document.createElement('script');                    
 scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    
-vizElement.parentNode.insertBefore(scriptElement, vizElement);        
+vizElement.parentNode.insertBefore(scriptElement, vizElement);      
